0e8850bfa78ec354e8414eac65d935a4eeb42966
youhei-0009 <yauchi.youhei0009@gmail.com>
2024-05-24 20:34:43 +0900

    useNavigate

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,8 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Page1 = () => {
     const arr = [...Array(100).keys()];
     console.log(arr);
+
+    const history = useNavigate();
+    const OnClickDetailA = () => {history("/page1/detailA")};
     return (
         <>
             <div>
@@ -10,6 +13,8 @@ export const Page1 = () => {
                 <Link to={{pathname: "/page1/detailA"}} state={arr}>DetailA</Link>
                 <br />
                 <Link to="/page1/detailB">DetailB</Link>
+                <br />
+                <button onClick={OnClickDetailA}>DetailA</button>
             </div>
         </>
     );
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,12 +1,16 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const Page1DetailA = () => {
     const location = useLocation();
     console.log(location.state);
+
+    const navigate = useNavigate();
+    const onClickBack = () => navigate("/page1");
     return (
         <>
             <div>
                 <h1>Page1 DetailA</h1>
+                <button onClick={onClickBack}>back</button>
             </div>
         </>
     );
